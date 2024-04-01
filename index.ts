1223b88c9473efe5fb0652b8845159ef441078f2
ash-codiology <ashfachohan7@gmail.com>
2024-04-01 06:46:53 +0500

    push my number guessing game to github

---
+++ index.ts
@@ -0,0 +1,17 @@
+#! /usr/bin/env node
+import inquirer from "inquirer";
+const randomNumber = Math.floor(Math.random() * 6 + 1);
+console.log("!!Welcome to ash07 number guessing game!!");
+
+const answer = await inquirer.prompt([{
+    name:"userGuessNumber",
+    type: "number",
+    message: "please guess a  number between 1-6: ",
+},
+]);
+
+if(answer.userGuessNumber === randomNumber){
+    console.log("Congratulations! you guessed right number.")
+}else {
+    console.log("you guessed wrong number.")
+};
